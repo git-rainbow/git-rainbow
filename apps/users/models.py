7b9819449ab318266e26f88c6c667616c3290716
Junhwan Kim <hwan@linuxgeek.io>
2023-05-30 16:31:41 +0900

    backend: Add User table

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    github_id = models.CharField(max_length=100, unique=True)
+    username = models.CharField(max_length=100, null=True)
+    USERNAME_FIELD = 'github_id'
--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,5 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
