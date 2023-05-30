80fe462a231e9f441e5d3e75e4df819f96c35de7
Jihoon Seo <seojh@linuxgeek.io>
2023-05-30 14:29:11 +0900

    front, backend: Add index.html

--- apps/developer/urls.py
+++ apps/developer/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from apps.developer import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('apps.developer.urls'))
+    path('', views.main_page),
 ]
--- apps/developer/views.py
+++ apps/developer/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def main_page(request):
+    return render(request, 'index.html')
+
