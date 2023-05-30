7622b7bd59d18f4b4a1b633eaaebf046bf4177f2
Junhwan Kim <hwan@linuxgeek.io>
2023-05-30 16:40:25 +0900

    backend: Add GithubUser table

--- apps/tech_stack/models.py
+++ apps/tech_stack/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class GithubUser(models.Model):
+    user = models.ForeignKey('users.User', on_delete=models.SET_NULL, null=True)
+    github_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=260, null=True)
+    email = models.EmailField(null=True)
+    avatar_url = models.CharField(max_length=100, null=True, blank=True)
+    bio = models.TextField(null=True)
+    status = models.CharField(max_length=30, null=True)
+    updated_date = models.DateTimeField(auto_now=True)
