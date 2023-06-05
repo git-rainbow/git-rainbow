705e482a86c1155425c5740442cec2b41e5a7f93
Taeung Song <taeung@linuxgeek.io>
2023-06-05 07:06:23 +0000

    backend settings: Allow all hosts

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = django_secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
