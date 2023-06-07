bcce74c89d394b9bf52f56f329e1151019650790
Junhwan Kim <hwan@linuxgeek.io>
2023-06-07 14:13:06 +0900

    backend: Export and rename functions that making data to utils.py

--- apps/developer/views.py
+++ apps/developer/views.py
@@ -1,17 +1,13 @@
-import os
-import random
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.template import loader
 from django.utils import timezone
-from django.db.models import Sum
-from django.db.models.functions import Trunc
 from dateutil.relativedelta import relativedelta
 from apps.tech_stack.models import GithubUser, TechStackFile
+from apps.tech_stack.utils import make_tech_card_data, make_calendar_data
 from config.local_settings import token_list
 from utils.core_func.core_func import core_repo_list
 from utils.github_api import request_github_profile
-from utils.git_analysis.calendar import git_calendar_colors
 
 
 def main_page(request):
@@ -46,44 +42,6 @@ def loading_page(request, github_id):
     return render(request, 'loading.html', context)
 
 
-def make_calendar_tech_data(tech_stack_files):
-    tech_name_list = tech_stack_files.exclude(tech_type='Data').distinct().values_list('tech_name', flat=True)
-    tech_data = {tech: 0 for tech in tech_name_list}
-    total_line = 0
-    for file in tech_stack_files:
-        if tech_data.get(file.tech_name) is not None:
-            tech_data[file.tech_name] += file.lines
-            total_line += file.lines
-    tech_data = sorted(tech_data.items(), key=lambda x: x[1], reverse=True)[:8]
-    calendar_tech_data = []
-    for tech_name, lines in tech_data:
-        percent = round(lines / total_line * 100, 1)
-        if percent > 0:
-            name_for_file = tech_name.lower()
-            tech = {'name': tech_name, 'file': name_for_file,
-                    'color': git_calendar_colors.get(tech_name, 'rgba(7,141,169,1.0)'),
-                    'percent': percent}
-            img_list = os.listdir('static/img')
-            if (f'{name_for_file}.png') not in img_list:
-                tech['file'] = f'none{random.randint(1, 3)}'
-            calendar_tech_data.append(tech)
-    return calendar_tech_data
-
-
-def make_profile_calendar_data(tech_files):
-    commit_date = tech_files.annotate(date=Trunc('author_date', 'day')).values('date').distinct()
-    calendar_data = {}
-    for date in commit_date:
-        data = tech_files.exclude(tech_type='Data') \
-            .filter(author_date__range=[date['date'], date['date'].replace(hour=23, minute=59, second=59)]) \
-            .values("tech_name") \
-            .annotate(lines=Sum('lines')) \
-            .order_by('-lines')
-        commit_data = dict(data.values_list('tech_name','lines')) if dict(data.values_list('tech_name','lines')) else 0
-        calendar_data[date['date'].strftime("%Y-%m-%d")] = commit_data
-    return calendar_data
-
-
 def analyze_page(request):
     if request.method != 'POST':
         return JsonResponse({"status":"Not allowed method"})
@@ -101,10 +59,10 @@ def analyze_page(request):
     # data for calendar
     today = timezone.now()
     tech_files = TechStackFile.objects.filter(github_id_id=github_id, author_date__range=[today - relativedelta(years=1), today])
-    calendar_tech_data = make_calendar_tech_data(tech_files)
-    calendar_data = make_profile_calendar_data(tech_files)
+    tech_card_data = make_tech_card_data(tech_files)
+    calendar_data = make_calendar_data(tech_files)
 
-    context = {'github_user':github_user, 'calendar_tech_data': calendar_tech_data,}
+    context = {'github_user':github_user, 'tech_card_data': tech_card_data,}
     json_data = {
         'status': user_status,
         'calendar_data': calendar_data
---
+++ apps/tech_stack/utils.py
@@ -0,0 +1,43 @@
+import os
+import random
+from django.db.models import Sum
+from django.db.models.functions import Trunc
+from utils.git_analysis.calendar import git_calendar_colors
+
+
+def make_tech_card_data(tech_stack_files):
+    tech_name_list = tech_stack_files.exclude(tech_type='Data').distinct().values_list('tech_name', flat=True)
+    tech_data = {tech: 0 for tech in tech_name_list}
+    total_line = 0
+    for file in tech_stack_files:
+        if tech_data.get(file.tech_name) is not None:
+            tech_data[file.tech_name] += file.lines
+            total_line += file.lines
+    tech_data = sorted(tech_data.items(), key=lambda x: x[1], reverse=True)[:8]
+    tech_card_data = []
+    for tech_name, lines in tech_data:
+        percent = round(lines / total_line * 100, 1)
+        if percent > 0:
+            name_for_file = tech_name.lower()
+            tech = {'name': tech_name, 'file': name_for_file,
+                    'color': git_calendar_colors.get(tech_name, 'rgba(7,141,169,1.0)'),
+                    'percent': percent}
+            img_list = os.listdir('static/img')
+            if (f'{name_for_file}.png') not in img_list:
+                tech['file'] = f'none{random.randint(1, 3)}'
+            tech_card_data.append(tech)
+    return tech_card_data
+
+
+def make_calendar_data(tech_files):
+    commit_date = tech_files.annotate(date=Trunc('author_date', 'day')).values('date').distinct()
+    calendar_data = {}
+    for date in commit_date:
+        data = tech_files.exclude(tech_type='Data') \
+            .filter(author_date__range=[date['date'], date['date'].replace(hour=23, minute=59, second=59)]) \
+            .values("tech_name") \
+            .annotate(lines=Sum('lines')) \
+            .order_by('-lines')
+        commit_data = dict(data.values_list('tech_name','lines')) if dict(data.values_list('tech_name','lines')) else 0
+        calendar_data[date['date'].strftime("%Y-%m-%d")] = commit_data
+    return calendar_data
