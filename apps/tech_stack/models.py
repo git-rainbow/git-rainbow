d7d2a115eb42de1c5aa2603d6360f0e65e821111
Taeung Song <taeung@linuxgeek.io>
2023-07-06 09:07:57 +0000

    model: Use ForeignKey for Ranking table

--- apps/tech_stack/models.py
+++ apps/tech_stack/models.py
@@ -34,7 +34,7 @@ class GithubCalendar(models.Model):
 
 
 class Ranking(models.Model):
-    github_id = models.OneToOneField('GithubUser', on_delete=models.CASCADE)
+    github_id = models.ForeignKey('GithubUser', on_delete=models.CASCADE)
     tech_name = models.CharField(max_length=50)
     midnight_rank = models.IntegerField()
-    updated_date = models.DateTimeField()
+    updated_date = models.DateTimeField(auto_now=True)
