285228f9cb514757d28c0f34828afd5a7f3e9a89
Junhwan Kim <hwan@linuxgeek.io>
2023-07-31 13:23:17 +0900

    frontend: Show total lines in ranking all page with intcomma

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'apps.developer',
     'apps.users',
     'apps.tech_stack',
