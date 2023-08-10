340c20081b627d602468e0bd05271244b78cdb00
Jihoon Seo <seojh@linuxgeek.io>
2023-08-10 17:49:25 +0900

    backend: Exception handling when token github_api fails

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,9 @@ def get_token():
     token = None
     for index in range(len(token_list)):
         res = github_rest_api(token_list[index], 'rate_limit')
-        if res['resources']['core']['remaining'] > 200:
+        if not res.get('resources'):
+            continue
+        elif res['resources']['core']['remaining'] > 200:
             token = token_list[index]
             break
     return token
