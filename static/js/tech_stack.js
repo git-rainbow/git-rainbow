3168343b4a4d3e88422ed9060e3e469085f61963
Junhwan Kim <hwan@linuxgeek.io>
2023-09-04 13:50:22 +0900

    front: Remove unnecessary javascript functions in tech_stack.js

--- static/js/tech_stack.js
+++ static/js/tech_stack.js
@@ -123,31 +123,6 @@ function check_analysis_updating(github_id, status){
     } 
 }
 
-function save_token(){
-    const target_user = location.pathname.replace('/', '');
-    const token = document.querySelector("#token_input").value;
-    const close_btn = document.querySelector("#close_btn");
-
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-
-    $.ajax({
-        url: '/save-token',
-        method: 'POST',
-        data: {'target_user': target_user, 'token': token},
-        async: true,
-        success: function (data) {
-            alert(data.response);
-            close_btn.click();
-        }
-    });
-}
-
 function is_valid_githubuser(github_id){
     let is_valid = false;
     let error_message;
@@ -199,72 +174,6 @@ function copy_svg_url(github_id){
         window.open(svg_url, "_blank");
     });
     document.body.removeChild(textarea);
-}
-
-function admin_tech_list_manage(action){
-    let tech_name_list = [];
-    if (action != 'create'){
-        let checkboxs = $("input[name=checkbox]:checked");
-        if(!$("input[name=checkbox]").is(":checked")){
-            alert('체크된 값이 없습니다.');
-            return;
-        }
-        checkboxs.each(function(i) {
-            tech_name_list.push(checkboxs[i].id);
-        });
-    }
-
-    let data = {'action': action};
-    if (action == 'create'){
-        let input_tech_name = document.querySelector(`#input_tech_name`).value;
-        let input_tech_type = document.querySelector(`#input_tech_type`).value;
-        if (!input_tech_name || !input_tech_type){
-            alert('기술명, 기술종류를 확인해주세요');
-            return;
-        }
-        data.tech_name = input_tech_name;
-        data.tech_type = input_tech_type;
-    } else if (action == 'update'){
-        let update_tech_date_list = [];
-        tech_name_list.forEach(tech_name => {
-            let tech_data = {
-                'tech_name': tech_name,
-                'tech_files_lists': document.querySelector(`#${tech_name}_tech_files_lists`).value,
-                'tech_files_keywords_lists': document.querySelector(`#${tech_name}_tech_files_keywords_lists`).value,
-                'ext_lists': document.querySelector(`#${tech_name}_ext_lists`).value,
-                'include_filter': document.querySelector(`#${tech_name}_include_filter`).value,
-                'keyword_lists': document.querySelector(`#${tech_name}_keyword_lists`).value,
-                'package_list': document.querySelector(`#${tech_name}_package_list`).value,
-                'is_special_tech_files': document.querySelector(`#${tech_name}_is_special_tech_files`).checked,
-            };
-            update_tech_date_list.push(tech_data);
-        });
-        data.update_tech_date_list = JSON.stringify(update_tech_date_list);
-    } else {
-        let delete_confirm = confirm("기술을 삭제하시겠습니까?");
-        if (delete_confirm){
-            data.delete_tech_list = JSON.stringify(tech_name_list);
-        }
-    }
-    $.ajaxSetup({
-        beforeSend: function (xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        }
-    });
-
-    $.ajax({
-        url: '/tech-list/edit'
-        ,method: 'POST'
-        ,data: data
-        ,async: false
-        ,success: function (data) {
-            if(data.status == "success"){
-                location.href = '/tech-list';
-            }
-        },
-    });
 }
 
 function go_to_login() {
