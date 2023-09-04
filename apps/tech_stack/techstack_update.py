24aa7b0e633c28bd712d1c444308fa1fd25c6b95
Junhwan Kim <hwan@linuxgeek.io>
2023-09-04 17:10:19 +0900

    backend: Improve getting data by using python codes instead of ORM distint option
    before
      total time: 8576 ms
      query time: 8033 ms
    
    after
      total time: 3203 ms
      query time: 2567 ms

--- apps/tech_stack/techstack_update.py
+++ apps/tech_stack/techstack_update.py
@@ -1,23 +1,30 @@
+from django.utils import timezone
+from collections import defaultdict
+from dateutil.relativedelta import relativedelta
 from django.dispatch import Signal, receiver
-from django.db.models import Count
+from django.db.models import Max
 from apps.tech_stack.models import GithubCalendar, TechStack
 from utils.github_calendar_colors.github_calendar_colors import github_calendar_colors
 
 
 @receiver(Signal())
 def update_techstack_table(**kwargs):
+    today = timezone.now()
+    year_ago = (today - relativedelta(years=1)).replace(hour=0, minute=0, second=0)
     tech_with_developer_count_query_dict = GithubCalendar.objects.\
-        filter(lines__gt=0, tech_name__in=github_calendar_colors.keys()).\
-        values('tech_name').\
-        annotate(developer_count=Count('github_id', distinct=True))
-    tech_developer_count_dict = {tech_data['tech_name']: tech_data['developer_count'] for tech_data in tech_with_developer_count_query_dict}
+        filter(author_date__gte=year_ago, lines__gt=0, tech_name__in=github_calendar_colors.keys()).\
+        values('tech_name', 'github_id').\
+        annotate(one_github_id=Max('github_id'))
+    tech_developer_set_dict = defaultdict(set)
+    for tech_data in tech_with_developer_count_query_dict:
+        tech_developer_set_dict[tech_data["tech_name"]].add(tech_data["github_id"])
 
     updated_tech_list = []
-    for tech_name, developer_count in tech_developer_count_dict.items():
+    for tech_name, developer_set in tech_developer_set_dict.items():
         _, created = TechStack.objects.update_or_create(
             tech_name = tech_name,
             defaults={
-                'developer_count': developer_count,
+                'developer_count': len(developer_set),
                 'tech_type': github_calendar_colors[tech_name]['tech_type'],
                 'tech_color': github_calendar_colors[tech_name]['tech_color'],
                 'logo_path': github_calendar_colors[tech_name]['logo_path']
