562ed5be5e940a3cbfda5e6db35ec128b97acd37
Jihoon Seo <seojh@linuxgeek.io>
2023-09-21 15:20:21 +0900

    backend model: Change update_date to access_date

--- apps/tech_stack/models.py
+++ apps/tech_stack/models.py
@@ -26,7 +26,7 @@ class GithubUser(models.Model):
 
 class GithubRepo(models.Model):
     github_id = models.ForeignKey("GithubUser", on_delete=models.CASCADE)
-    updated_date = models.DateTimeField(auto_now=True, null=True)
+    access_date = models.DateTimeField(null=True)
     repo_url = models.CharField(max_length=260)
     branch = models.CharField(max_length=260, null=True)
     description = models.TextField(null=True)
