80ba808d32367955d030f1b4f5aa4618e8603aab
Jihoon Seo <seojh@linuxgeek.io>
2023-09-22 17:43:38 +0900

    front backend: Display user request data

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -1,8 +1,12 @@
+from django.db.models import Count
 from django.shortcuts import render
 from django.utils import timezone
 
+from apps.admins.models import PageRequest
 from apps.tech_stack.models import GithubUser
 from apps.users.models import User
+
+from collections import defaultdict
 
 
 def admin_page(request):
@@ -17,6 +21,13 @@ def admin_page(request):
     all_new_member = all_member.filter(date_joined__range=[first_day_of_this_month, now])
     all_new_member_cnt = all_new_member.count()
 
+    page_request_data = PageRequest.objects.all().values('request_type', 'date', 'count').annotate(
+        unique_cnt=Count('access_client')
+    ).order_by('request_type')
+    access_data = defaultdict(lambda: {'count': 0, 'unique_cnt': 0})
+    for data in page_request_data:
+        access_data[data['request_type']]['count'] += data['count']
+        access_data[data['request_type']]['unique_cnt'] += data['unique_cnt']
     context = {
         'all_member': all_member,
         'all_member_cnt': all_member_cnt,
@@ -24,5 +35,6 @@ def admin_page(request):
         'all_githubuser_cnt': all_githubuser_cnt,
         'all_new_member': all_new_member,
         'all_new_member_cnt': all_new_member_cnt,
+        'access_data': dict(access_data),
     }
     return render(request, 'admin/admin_dashboard.html', context)
