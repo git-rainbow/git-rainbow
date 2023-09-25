8426d31278a24aee7d4ed0739cff64a958e94d08
Junhwan Kim <hwan@linuxgeek.io>
2023-09-25 14:54:42 +0900

    frontend: Handle execption when tech is null in search/repos

--- static/js/search_repo.js
+++ static/js/search_repo.js
@@ -56,6 +56,20 @@ function search_repo(isNewSearch, isWithToken, isFirstLoad) {
                 for (let topic of topics) {
                     insertTopicHtml += `<span class="topic-span">${topic}</span>`;
                 }
+                let item_language_part
+                if (item.language){
+                    item_language_part = `
+                    <div class="flex align-center">
+                      <div style="background-color: ${item.language ? github_calendar_colors(item.language, 1): 'white'}; width: 10px; height: 10px; border-radius: 50%;"></div>
+                      <div>${item.language}</div>
+                    </div>
+                    <div class="flex align-center">
+                      ·
+                    </div>
+                    `;
+                } else {
+                    item_language_part = '';
+                }
                 const repoHtml = `
                 <div class="repo-box mb-2" style="margin:auto">
                   <div>
@@ -71,13 +85,7 @@ function search_repo(isNewSearch, isWithToken, isFirstLoad) {
                     ${insertTopicHtml}
                   </div>
                   <div class="flex">
-                    <div class="flex align-center">
-                      <div style="background-color: ${item.language ? github_calendar_colors(item.language, 1): 'white'}; width: 10px; height: 10px; border-radius: 50%;"></div>
-                      <div>${item.language}</div>
-                    </div>
-                    <div class="flex align-center">
-                      ·
-                    </div>
+                    ${item_language_part}
                     <div class="flex align-center">
                       <svg aria-hidden="true" focusable="false" role="img" class="octicon octicon-star" viewBox="0 0 16 16" width="16" height="16" fill="currentColor" style="display: inline-block; user-select: none; vertical-align: text-bottom; overflow: visible;"><path d="M8 .25a.75.75 0 0 1 .673.418l1.882 3.815 4.21.612a.75.75 0 0 1 .416 1.279l-3.046 2.97.719 4.192a.751.751 0 0 1-1.088.791L8 12.347l-3.766 1.98a.75.75 0 0 1-1.088-.79l.72-4.194L.818 6.374a.75.75 0 0 1 .416-1.28l4.21-.611L7.327.668A.75.75 0 0 1 8 .25Zm0 2.445L6.615 5.5a.75.75 0 0 1-.564.41l-3.097.45 2.24 2.184a.75.75 0 0 1 .216.664l-.528 3.084 2.769-1.456a.75.75 0 0 1 .698 0l2.77 1.456-.53-3.084a.75.75 0 0 1 .216-.664l2.24-2.183-3.096-.45a.75.75 0 0 1-.564-.41L8 2.694Z"></path></svg>
                       <span>&nbsp${item.stargazers_count}</span>
