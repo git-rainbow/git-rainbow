6a3d5fc28e79d36a160b4d31a4ece67e65f897d2
Junhwan Kim <hwan@linuxgeek.io>
2023-11-07 16:25:53 +0900

    admin: Modify displaying admins page depending on tech logo and unique count

--- apps/admins/views.py
+++ apps/admins/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from django.utils import timezone
 
 from apps.admins.models import PageRequest, ClientInfo
-from apps.tech_stack.models import GithubUser
+from apps.tech_stack.models import GithubUser, TechStack
 from apps.users.models import User
 
 from collections import defaultdict
@@ -20,22 +20,37 @@ def admin_page(request):
 
     all_githubuser = list(GithubUser.objects.filter(user__isnull=True).values('avatar_url', 'github_id', 'updated_date').order_by('-updated_date')[:1000])
     all_githubuser_cnt = GithubUser.objects.filter(user__isnull=True).count()
+    tech_name_list = TechStack.objects.values_list('tech_name', flat=True)
+    lower_tech_name_list = []
+    request_type_list = ['github_id', 'group', 'index', 'ranking', 'search']
 
-    page_request_data = PageRequest.objects.all().values('request_type', 'date', 'count').annotate(
+    for tech in tech_name_list:
+        if tech == 'C#':
+            tech = 'c_sharp'
+        lower_tech_name = tech.lower()
+        lower_tech_name_list.append(lower_tech_name)
+        request_type_list.append(f'ranking/{lower_tech_name}')
+    page_request_data = PageRequest.objects.filter(request_type__in=request_type_list).values('request_type', 'date', 'count').annotate(
         unique_cnt=Count('access_client')
-    ).order_by('request_type')
-    access_data = defaultdict(lambda: {'count': 0, 'unique_cnt': 0, 'access_user_data': []})
+    )
+    access_data = defaultdict(lambda: {'count': 0, 'unique_cnt': 0, 'access_user_data': [], 'logo_path': None})
     for data in page_request_data:
         access_data[data['request_type']]['count'] += data['count']
         access_data[data['request_type']]['unique_cnt'] += data['unique_cnt']
+        if data['request_type'].startswith('ranking/'):
+            tech_name = data['request_type'].split('ranking/')[1]
+            if tech_name in lower_tech_name_list:
+                logo_path = f'/static/img/{tech_name}.png'
+                access_data[data['request_type']]['logo_path'] = logo_path
 
     access_client_queryset = ClientInfo.objects.select_related('user')
-    request_data_list = list(PageRequest.objects.prefetch_related(
+    request_data_list = list(PageRequest.objects.filter(request_type__in=request_type_list).prefetch_related(
         Prefetch('access_client', queryset=access_client_queryset)
-    ).all().order_by('-date'))
+    ).all())
     for request_data in request_data_list:
         for client in request_data.access_client.all():
             access_data[request_data.request_type]['access_user_data'].append({request_data.date: client})
+    access_data = sorted(access_data.items(), key=lambda x: (-x[1]['unique_cnt'], x))
 
     context = {
         'all_member': all_member,
